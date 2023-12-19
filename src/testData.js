ed522f2e1f82d412dbfa04b624d4388d511138dd
Mandip Chhetri <108076760+Mandip47@users.noreply.github.com>
2023-12-19 13:17:51 +0000

    works for fake data

--- src/testData.js
+++ src/testData.js
@@ -1,36 +1,35 @@
-// const express = require("express");
-// const http = require("http");
-// const io = require("socket.io");
+const express = require("express");
+const http = require("http");
+const io = require("socket.io");
 
-// const app = express();
-// const server = http.createServer(app);
-// const socketIO = io(server, {
-//   transports: ["websocket"], // Use only WebSocket transport
-// });
+const app = express();
+const server = http.createServer(app);
+const socketIO = io(server);
+app.use(express.static(__dirname + "/public"));
 
-// app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/index.html");
-// });
+socketIO.on("connection", (socket) => {
+  console.log("User connected", socket.id);
 
-// socketIO.on("connection", (socket) => {
-//   console.log("User connected", socket.id);
+  // Send fake data to the client at regular intervals
+  const fakeDataInterval = setInterval(() => {
+    const fakeData = Math.floor(Math.random() * (110 - 10 + 1)) + 10;
+    // const fakeData = Math.floor(Math.random() * (10 - 10 + 5));
+    const fakeInfoData = Math.round(Math.random());
+    socket.emit("fakeData", { value: fakeData , info: fakeInfoData});
+  }, 2000);
 
-//   // Send fake data to the client at regular intervals
-//   const fakeDataInterval = setInterval(() => {
-//     const fakeData = Math.random();
-//     socket.emit("fakeData", { value: fakeData });
-//   }, 1000);
+  // Stop sending fake data when the client disconnects
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+    clearInterval(fakeDataInterval);
+  });
+});
 
-//   // Stop sending fake data when the client disconnects
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected", socket.id);
-//     clearInterval(fakeDataInterval);
-//   });
-// });
-
-// const port = 5500;
-// server.listen(port, () => {
-//   console.log(`Server is listening on port ${port}`);
-// });
+const port = 5500;
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
